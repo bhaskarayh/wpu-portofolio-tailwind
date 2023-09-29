55b17e35b7903bbaa0ba5f54792f38f3e5c0f998
bhaskarayh <bhaskarayh@gmail.com>
2023-09-29 11:31:09 +0700

    add dark mode mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,24 +1,44 @@
 // Navbar Fixed
 window.onscroll = () => {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
-
-    // console.log({ fixedNav, pageYOffset: window.pageYOffset });
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
-    }
-}
-
+  // console.log({ fixedNav, pageYOffset: window.pageYOffset });
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.add("flex");
+    toTop.classList.remove("hidden");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
+  }
+};
 
 // Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+// Click outside hamburger
+window.addEventListener("click", (e) => {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Darkmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", () => {
+  darkToggle.checked
+    ? html.classList.add("dark")
+    : html.classList.remove("dark");
 });
